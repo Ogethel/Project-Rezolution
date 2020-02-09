41d557b80bec0be2b2fe8da6e893af3cb05cf22d
McKade <10718916@my.uvu.edu>
2020-02-09 00:30:33 -0700

    Adding Platform Functionality

---
+++ Project Rezolution/Assets/Scripts/Data Assets/EventsSystemTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventsSystemTest : MonoBehaviour
+{
+    /*OntriggerEnter(collider other)
+     * What happens with it triggers
+     * define variable
+     */
+
+    public UnityEvent EnterEvent, ExitEvent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        EnterEvent.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        ExitEvent.Invoke();
+    }
+
+}
---
+++ Project Rezolution/Assets/Scripts/Platforms/PlayerOnPlatform.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[RequireComponent(typeof(Animator))]
+
+public class PlayerOnPlatform : MonoBehaviour
+{
+    public GameObject Player;
+    public Animator platAnimator;
+
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            Player.transform.parent = transform;
+        }
+    }
+
+   
+   
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            Player.transform.parent = null;
+        }
+    }
+}
