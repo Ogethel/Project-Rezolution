72f239db67f3059c5fb0592d05c7deb0d52582b5
Ogethel <10718916@my.uvu.edu>
2020-03-10 15:37:46 -0600

    Arranging my New Map + Collections w/Anthony

---
+++ Project Rezolution/Assets/Scripts/Scriptable Objects/Collection.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class Collection : ScriptableObject
+{
+    public List<Collectable> collectablesList;
+    public int currentCollectableNum;
+
+    public void AddToCollection(Collectable collecatbleObj)
+    {
+        if (collectablesList.Contains(collecatbleObj))
+        {
+            return;
+            collectablesList.Add(collecatbleObj);
+        }
+        
+    }
+
+    public void RemoveFromCollection(Collectable collecatbleObj)
+    {
+        for (int i = 0; i < collectablesList.Count; i++)
+        {
+            Collectable obj = collectablesList[i];
+            if (obj == collecatbleObj)
+            {
+                collectablesList.Remove(collecatbleObj);
+            }
+        }
+    }
+
+
+    public void ClearCollection()
+    {
+        collectablesList.Clear();
+    } 
+
+    public void UseCurrentItem()
+    {
+        if (collectablesList.Capacity > 0)
+        {
+            collectablesList[currentCollectableNum].Use();
+        }
+    }
+
+    public void IncrementCurrentNum()
+    {
+        if (currentCollectableNum < collectablesList.Count + 1)
+        {
+            currentCollectableNum++;
+        }
+        else
+        {
+            currentCollectableNum = 0;
+        }
+    }
+}
---
+++ Project Rezolution/Assets/Scripts/Scriptable Objects/WeaponCollectable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+
+public class WeaponCollectable : Collectable
+{
+    public UnityEvent useEvent;
+
+    public override void Use()
+    {
+        useEvent.Invoke();
+    }
+
+    public void Attack()
+    {
+        Debug.Log("Attack" + this);
+    }
+
+    public void Equip()
+    {
+
+    }
+
+   
+}
